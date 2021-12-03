74c355372a4048f0df12935547abb7221efe2f56
Maik <maycolp88@icloud.com>
2021-12-02 21:00:12 -0500

    PVTPEDIA V1

---
+++ Aplicativos/PVTPEDIA/src/PVT/Operaciones.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PVT;
+
+/**
+ *
+ * @author EQUIPO
+ */
+public class Operaciones {
+
+    String a, b, c;
+    float x, y, z;
+
+    public boolean getCheckempty(String a, String b, String c) {
+        if (a.equals("") || b.equals("") || c.equals("") || a.equals("") && b.equals("") && c.equals("")) {
+            return true;
+        } else {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            return false;
+        }
+    }
+
+    public boolean getCheckIsNumeric() {
+        try {
+            Float.parseFloat(a);
+            Float.parseFloat(b);
+            Float.parseFloat(c);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void Convertir() {
+        this.x = Float.parseFloat(a);
+        this.y = Float.parseFloat(b);
+        this.z = Float.parseFloat(c);
+    }
+}
